cbce1f6b8678e155058b3dba22a5f170c8daedcf
mejdoubi meriem <mejdoubi.meriem@gmail.Com>
2025-03-13 15:02:13 +0100

    Add files via upload

---
+++ ContactForm.php
@@ -0,0 +1,47 @@
+<?php
+namespace Drupal\mmej_formulaire\Form;
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+/**
+ * Formulaire de contact personnalisé.
+ */
+class ContactForm extends FormBase {
+  /**
+   * Identifiant unique du formulaire.
+   */
+  public function getFormId() {
+    return 'mmej_contact_form';
+  }
+  /**
+   * Construire le formulaire.
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $form['nom'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Nom'),
+      '#required' => TRUE,
+    ];
+    $form['email'] = [
+      '#type' => 'email',
+      '#title' => $this->t('Email'),
+      '#required' => TRUE,
+    ];
+    $form['message'] = [
+      '#type' => 'textarea',
+      '#title' => $this->t('Message'),
+      '#required' => TRUE,
+    ];
+    $form['actions']['submit'] = [
+      '#type' => 'submit',
+      '#value' => $this->t('Envoyer'),
+    ];
+    return $form;
+  }
+  /**
+   * Gérer la soumission du formulaire.
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    \Drupal::messenger()->addMessage($this->t('Merci @name, votre message a été envoyé !', ['@name' => $form_state->getValue('nom')]));
+  }
+}
+?>
